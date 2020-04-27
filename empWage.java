0ed353392be4b57e8bce13db61b3411febd42232
RAVI26-B01 <ravi26.b01@gmail.com>
2020-04-27 21:59:15 +0530

    added part_time

--- empWage.java
+++ empWage.java
@@ -3,15 +3,24 @@ import java.lang.*;
 public class empWage {
         public static void main(String[] args) {
                 int is_full_time=1;
-                double emp_check=Math.floor(Math.random()*10)%2;
+                int is_part_time=2;
+                int emp_rate_per_hour=20;
+                int emp_hours;
+               double emp_check=Math.floor(Math.random()*10)%3;
+
                 if(emp_check==is_full_time){
-			int emp_rate_per_hour=20;
-			int emp_hours=8;
-                        int Salary=(emp_rate_per_hour*emp_hours);
-			System.out.println(Salary);
+                        emp_hours=8;
                 }
+
+                else if(emp_check==is_part_time){
+                        emp_hours=4;
+                }
+
                 else {
-                        System.out.println(0);
+                        emp_hours=0;
                 }
+                int salary=emp_rate_per_hour*emp_hours;
+                System.out.println(salary);
         }
 }
+
