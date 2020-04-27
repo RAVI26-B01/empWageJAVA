61400980b77e1c41a037fe8d2967736d090c980f
RAVI26-B01 <ravi26.b01@gmail.com>
2020-04-27 22:11:52 +0530

    using switch_case

--- empWage.java
+++ empWage.java
@@ -5,20 +5,18 @@ public class empWage {
                 int is_full_time=1;
                 int is_part_time=2;
                 int emp_rate_per_hour=20;
-                int emp_hours;
-               double emp_check=Math.floor(Math.random()*10)%3;
+                int emp_hours=0;
+                int emp_check=(int)Math.floor(Math.random()*10)%3;
 
-                if(emp_check==is_full_time){
-                        emp_hours=8;
-                }
+		switch(emp_check){
+			case 1: emp_hours=8;
+				break;
+			case 2: emp_hours=4;
+				break;
+			case 0: emp_hours=0;
+				break;
+		}
 
-                else if(emp_check==is_part_time){
-                        emp_hours=4;
-                }
-
-                else {
-                        emp_hours=0;
-                }
                 int salary=emp_rate_per_hour*emp_hours;
                 System.out.println(salary);
         }
